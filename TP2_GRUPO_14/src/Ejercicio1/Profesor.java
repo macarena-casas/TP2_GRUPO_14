3016e24fb3b36176730ce0d708628ae1a1d5fe1a
AlejandroBeron <ale.frecuenci@gmail.com>
2024-08-22 21:19:32 -0300

    Agrego constructores y atributos de profesor.

--- TP2_GRUPO_14/src/Ejercicio1/Profesor.java
+++ TP2_GRUPO_14/src/Ejercicio1/Profesor.java
@@ -4,5 +4,31 @@ public class Profesor extends Empleado {
 
 	String cargo;
 	int antiguedadDocente;
+	
+	public Profesor() {
+		
+		cargo ="sin cargo";
+		antiguedadDocente = 0;
+	}
+	
+	public Profesor(String cargo, int antiguedadDocente) {
+		super();
+		this.cargo = cargo;
+		this.antiguedadDocente = antiguedadDocente;
+	}
+	
+	public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	public int getAntiguedadDocente() {
+		return antiguedadDocente;
+	}
+	public void setAntiguedadDocente(int antiguedadDocente) {
+		this.antiguedadDocente = antiguedadDocente;
+	}
 
+	
 }
