bb0c009a477e3a57d4e84a92ae8b06414f147763
Facundo <124022262+FacundoDorrego@users.noreply.github.com>
2024-08-29 23:16:18 -0300

    Modificacion MainEjercicio3

--- TP2_GRUPO_14/src/Ejercicio3/mainEjercicio3.java
+++ TP2_GRUPO_14/src/Ejercicio3/mainEjercicio3.java
@@ -1,41 +1,37 @@
 package Ejercicio3;
-
 import java.util.ArrayList;
-import java.util.Iterator;
-
-
+import java.util.ListIterator;
 
 public class mainEjercicio3 {
 
 	public static void main(String[] args) {
-		ArrayList<Polideportivo> listaPolideportivo = new ArrayList<Polideportivo>(5);
-		listaPolideportivo.add(new Polideportivo(1000,"Los Troncos"));
-		listaPolideportivo.add(new Polideportivo(1500,"El Zorzal"));
-		listaPolideportivo.add(new Polideportivo(1600,"General Pacheco"));
-	
-Iterator<Polideportivo> it = listaPolideportivo.iterator();
 		
-		while(it.hasNext())
-		{
-			Polideportivo p = (Polideportivo) it.next();
-			System.out.println(p.toString());
+		ArrayList<Iedificio> instalaciones = new ArrayList<>();
+		
+		// 3 polideportivos
+		Polideportivo poli1 = new Polideportivo(100 , "Polideportivo Uno");
+		Polideportivo poli2 = new Polideportivo(200 , "Polideportivo Dos");
+		Polideportivo poli3 = new Polideportivo(300 , "Polideportivo Tres");
+		
+		// 2 edificios 
+		EdificioOficinas edi1 = new EdificioOficinas(1000, 10, 10);
+		EdificioOficinas edi2 = new EdificioOficinas(3000, 15, 5);
+		
+		//agregamos a la lista
+		
+		instalaciones.add(poli1);
+		instalaciones.add(poli2);
+		instalaciones.add(poli3);
+		instalaciones.add(edi1);
+		instalaciones.add(edi2);
+		
+		//iterator
+		
+		ListIterator<Iedificio> it = instalaciones.listIterator();
+		while (it.hasNext()) {
+			Iedificio edificio = it.next()	;
+			System.out.println(edificio.toString());
 		}
 		
-	
-			ArrayList<EdificioOficinas> listaEdificioOficina = new ArrayList<EdificioOficinas>(5);
-			listaEdificioOficina.add(new EdificioOficinas(30,50,6));
-			listaEdificioOficina.add(new EdificioOficinas(50,20,15));
-		
-	Iterator<EdificioOficinas> Eo = listaEdificioOficina.iterator();
-			
-			while(Eo.hasNext())
-			{
-				EdificioOficinas E = (EdificioOficinas) Eo.next();
-				System.out.println(E.toString());
-			}
-			
-		
-		
 	}
-
 }
